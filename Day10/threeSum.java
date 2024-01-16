1965ad4bd3ffc54257b58b6b541b218513faeea6
Phan Phat Tai <70010376+TaiTitans@users.noreply.github.com>
2024-01-16 13:26:14 +0700

    Update
    Day 10: 16/01/2023

---
+++ Day10/threeSum.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class threeSum{
+    public List<List<Integer>> threeSums(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sorting not same
+
+        for(int i = 0; i< nums.length - 2;i++){
+            if(i==0 || (i>0 && nums[i] != nums[i-1])){
+                int target = -nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+                while(left < right){
+                    int sum = nums[left] + nums[right];
+                    if(sum == target){
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                        while(left < right && nums[left] == nums[left+1]) left++;
+                        while(left < right && nums[right] == nums[right-1]) right--;
+                        left++;
+                        right--;
+                        
+                    }else if(sum < target){
+                        left++;
+                    }else{
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+
+public static void main(String[] args){
+    threeSum testSolution = new threeSum();
+    int[] nums = {-1,0,1,2,-1,-4};
+    List<List<Integer>> result = testSolution.threeSums(nums);
+    for(List<Integer> triplet : result){
+        System.out.println(triplet);
+    }
+}
+}
