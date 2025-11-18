da2a17a5129784faf6af8acabe8a15edaabeb529
Phan Phat Tai <taititansofficial@gmail.com>
2025-11-18 11:24:20 +0700

    feat: day33

---
+++ Day33/go.mod
@@ -0,0 +1,3 @@
+module Day33
+
+go 1.21.4
---
+++ Day33/main.go
@@ -0,0 +1,20 @@
+package main
+
+func isOneBitCharacter(bits []int) bool {
+	n := len(bits)
+	i := 0
+	for i < n-1 {
+		if bits[i] == 1 {
+			i += 2
+		} else {
+			i += 1
+		}
+	}
+	return i == n-1
+}
+
+func main() {
+	bits := []int{1, 0, 0}
+	result := isOneBitCharacter(bits)
+	println(result)
+}
